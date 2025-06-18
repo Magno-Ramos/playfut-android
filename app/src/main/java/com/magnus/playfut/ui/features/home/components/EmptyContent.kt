7b93a8aa79205f1a2253656c923676eb09cddb29
Magno <magno23ramos@gmail.com>
2025-06-18 11:37:31 -0300

    change empty state

--- app/src/main/java/com/magnus/playfut/ui/features/home/components/EmptyContent.kt
+++ app/src/main/java/com/magnus/playfut/ui/features/home/components/EmptyContent.kt
@@ -3,19 +3,13 @@ package com.magnus.playfut.ui.features.home.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.SportsScore
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -30,17 +24,10 @@ fun EmptyContent(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Icon(
-            imageVector = Icons.Outlined.SportsScore,
-            contentDescription = null,
-            tint = MaterialTheme.colorScheme.onBackground
-        )
-        Spacer(Modifier.height(8.dp))
         Text(
             text = "Você ainda não faz parte\n de nenhum grupo.",
             textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onBackground,
-            fontWeight = FontWeight.Bold
+            color = MaterialTheme.colorScheme.onBackground
         )
     }
 }
