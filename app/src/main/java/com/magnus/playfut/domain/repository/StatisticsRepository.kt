e13d647193f8e4fea6ce02818f04979337b4d966
Ana Ramos <anaramos020423@gmail.com>
2025-06-25 19:39:26 -0300

    change stats repository

--- app/src/main/java/com/magnus/playfut/domain/repository/StatisticsRepository.kt
+++ app/src/main/java/com/magnus/playfut/domain/repository/StatisticsRepository.kt
@@ -2,14 +2,12 @@ package com.magnus.playfut.domain.repository
 
 import com.magnus.playfut.domain.database.daos.GroupDao
 import com.magnus.playfut.domain.database.daos.PlayerDao
-import com.magnus.playfut.domain.database.daos.RoundDao
 import com.magnus.playfut.domain.model.structure.Artillery
 import com.magnus.playfut.ui.features.statistic.home.PlayerMostPresentHighlight
 import com.magnus.playfut.ui.features.statistic.home.PlayerMostWinsHighlight
 import com.magnus.playfut.ui.features.statistic.home.StatisticHomeViewState
 
 class StatisticsRepository(
-    private val roundDao: RoundDao,
     private val groupDao: GroupDao,
     private val playerDao: PlayerDao
 ) {
@@ -19,22 +17,20 @@ class StatisticsRepository(
     }
 
     suspend fun fetchStatistics(groupId: String): Result<StatisticHomeViewState> = runCatching {
+        val groupStats = groupDao.getGroupStats(groupId)
         val totalActivePlayersCount = playerDao.getActivePlayersCount(groupId.toLong())
-        val totalRounds = roundDao.getRoundCountByGroup(groupId)
-        val totalMatches = groupDao.getMatchesCount(groupId.toLong())
-        val totalGoals = groupDao.getTotalGoals(groupId.toLong())
         val artilleryRanking = playerDao.getPlayersScoreRanking(groupId)
 
         var playerMostPresent: PlayerMostPresentHighlight? = null
         var playerWithMostWins: PlayerMostWinsHighlight? = null
 
         // rule to show player with most wins
-        if (totalRounds > 2 && totalActivePlayersCount >= 5) {
+        if (groupStats.totalRounds > 2 && totalActivePlayersCount >= 5) {
             val list = playerDao.getPlayersWithMostWins(groupId)
             val topPlayer = list.firstOrNull()
             if (topPlayer != null) {
                 val hasGoodWinRate = topPlayer.winCount >= topPlayer.matchCount / 2
-                val hasRelevantParticipation = topPlayer.matchCount >= totalRounds / 2
+                val hasRelevantParticipation = topPlayer.matchCount >= groupStats.totalRounds / 2
                 if (hasGoodWinRate && hasRelevantParticipation) {
                     playerWithMostWins = PlayerMostWinsHighlight(
                         player = topPlayer.player.name,
@@ -46,22 +42,22 @@ class StatisticsRepository(
         }
 
         // rule to show player most present
-        if (totalRounds > 2 && totalActivePlayersCount >= 5) {
+        if (groupStats.totalRounds > 2 && totalActivePlayersCount >= 5) {
             val list = playerDao.getMostPresentPlayers(groupId)
             val topPlayer = list.firstOrNull()
             if (topPlayer != null) {
                 playerMostPresent = PlayerMostPresentHighlight(
                     player = topPlayer.player.name,
                     roundCount = topPlayer.roundCount,
-                    totalRounds = totalRounds
+                    totalRounds = groupStats.totalRounds
                 )
             }
         }
 
         StatisticHomeViewState(
-            totalRounds = totalRounds,
-            totalMatches = totalMatches,
-            totalGoals = totalGoals,
+            totalRounds = groupStats.totalRounds,
+            totalMatches = groupStats.totalMatches,
+            totalGoals = groupStats.totalScores,
             artilleryRanking = artilleryRanking,
             playerMostPresent = playerMostPresent,
             playerWithMostWins = playerWithMostWins
